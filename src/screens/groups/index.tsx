63944184358024c9b18f8ff42c97a0154890e107
Ian Santos <ian.santos@leste.com>
2024-01-03 17:51:29 -0300

    feat: GroupList

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,12 +1,27 @@
+import { Groupcard } from '@components/groupcard';
 import { Header } from '@components/header';
 import { Hightlight } from '@components/highlight';
+import React, { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    'Galera do Ignite',
+    'Galera da Rocket',
+  ]);
+
   return (
     <Container>
-      <Header></Header>
+      <Header />
       <Hightlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <FlatList
+        style={{ width: '90%' }}
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <Groupcard title={item} />}
+      />
     </Container>
   );
 }
