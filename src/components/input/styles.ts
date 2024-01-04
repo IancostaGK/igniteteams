d548dc593e41ea96255c3b4638dd1e844ef0d193
Ian Santos <ian.santos@leste.com>
2024-01-04 13:47:41 -0300

    feat: input component

---
+++ src/components/input/styles.ts
@@ -0,0 +1,17 @@
+import { ColorValue, TextInput } from 'react-native';
+import styled from 'styled-components';
+
+export const Container = styled(TextInput).attrs(({ theme }) => ({
+  placeholderTextColor: theme.colors.gray300 as ColorValue,
+}))`
+  flex: 1;
+  width: 90%;
+  min-height: 56px;
+  max-height: 56px;
+  border-radius: 6px;
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.gray700};
+  font-size: ${({ theme }) => theme.fontSize.md}px;
+  font-family: ${({ theme }) => theme.fontFamily.regular};
+  padding: 16px;
+`;
--- src/screens/newGroup/index.tsx
+++ src/screens/newGroup/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@components/button';
 import { Header } from '@components/header';
 import { Hightlight } from '@components/highlight';
+import { Input } from '@components/input';
 import React from 'react';
 import { Container, Content, Icon } from './styles';
 
@@ -15,7 +16,8 @@ export function NewGroup() {
           title="Nova Turma"
           subtitle="Crie uma turma para adicionar pessoas"
         />
-        <Button text="Criar" />
+        <Input placeholder="Nome da turma" />
+        <Button text="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );
